e652f8605fb81c588e9cd018f33d82a13b42d6df
Adesanmi Dayo PraiseGod <dayopraisegod@gmail.com>
2023-11-09 17:31:05 +0100

    Add typings to variables and constants

--- bikeshare.py
+++ bikeshare.py
@@ -1,16 +1,17 @@
 import time
+from typing import Any
 
 import numpy as np
 import pandas as pd
 
-CITY_DATA = {
+CITY_DATA: dict[str, str] = {
     "chicago": "chicago.csv",
     "new york city": "new_york_city.csv",
     "washington": "washington.csv",
 }
 
 # was generated with `df['day_of_week'].unique()` statement then add `All`
-DAYS_OPTION = [
+DAYS_OPTION: list[str] = [
     "all",
     "friday",
     "thursday",
@@ -22,10 +23,18 @@ DAYS_OPTION = [
 ]
 
 # was generated with `df['month'].unique()` statement then add `All`
-MONTHS_OPTIONS = ["all", "june", "may", "january", "march", "april", "february"]
+MONTHS_OPTIONS: list[str] = [
+    "all",
+    "june",
+    "may",
+    "january",
+    "march",
+    "april",
+    "february",
+]
 
 
-def handle_invalid_input(input_question: str, checklist: list) -> str:
+def handle_invalid_input(input_question: str, checklist: Any) -> str:
     """
     An helper function, helps to reduce code repetition.
     It handles the invalid inputs entered by users.
@@ -37,7 +46,7 @@ def handle_invalid_input(input_question: str, checklist: list) -> str:
     Returns:
         (str) choice - what the user entered at the prompt.
     """
-    choice = input(input_question).lower().strip()
+    choice: str = input(input_question).lower().strip()
     # handle invalid city inputs
     while choice not in checklist:
         print("Invalid input! Please, try again!\n\n")
@@ -59,14 +68,14 @@ def get_filters() -> tuple:
     """
     print("Hello! Let's explore some US bikeshare data!")
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-    month = "all"
-    day = "all"
-    city = handle_invalid_input(
+    month: str = "all"
+    day: str = "all"
+    city: str = handle_invalid_input(
         "Would you like to see data for Chicago, New York City, or Washington? ",
         CITY_DATA,
     )
     # get user input for month (all, january, february, ... , june)
-    filter_choice = (
+    filter_choice: str = (
         input(
             "Would you like to filter the data by month, day, both or not at all? Type 'none' for no time filter: "
         )
@@ -117,7 +126,7 @@ def load_data(city: str, month: str, day: str) -> pd.DataFrame:
         df - Pandas DataFrame containing city data filtered by month and day
     """
     # load the city data
-    df = pd.read_csv(CITY_DATA[city.lower()])
+    df: Any = pd.read_csv(CITY_DATA[city.lower()])
 
     # convert the "Start Time" column to datetime object
     df["Start Time"] = pd.to_datetime(df["Start Time"])
@@ -145,7 +154,7 @@ def time_stats(df: pd.DataFrame) -> None:
     """Displays statistics on the most frequent times of travel."""
 
     print("\nCalculating The Most Frequent Times of Travel...\n")
-    start_time = time.time()
+    start_time: float = time.time()
 
     # display the most common month
     print("The most frequent month of travel: {}".format(df["month"].mode()[0]))
@@ -168,14 +177,14 @@ def station_stats(df: pd.DataFrame) -> None:
     """Displays statistics on the most popular stations and trip."""
 
     print("\nCalculating The Most Popular Stations and Trip...\n")
-    start_time = time.time()
+    start_time: float = time.time()
 
     # display most commonly used start station
-    common_start_station = df["Start Station"].mode()[0]
+    common_start_station: str = df["Start Station"].mode()[0]
     print("The most commonly used start station: {}".format(common_start_station))
 
     # display most commonly used end station
-    common_end_station = df["End Station"].mode()[0]
+    common_end_station: str = df["End Station"].mode()[0]
     print("The most commonly used end station: {}".format(common_end_station))
 
     # display most frequent combination of start station and end station trip
@@ -193,7 +202,7 @@ def trip_duration_stats(df: pd.DataFrame) -> None:
     """Displays statistics on the total and average trip duration."""
 
     print("\nCalculating Trip Duration...\n")
-    start_time = time.time()
+    start_time: float = time.time()
 
     # display total travel time
     print("Total travel time: {} seconds".format(df["Trip Duration"].sum()))
@@ -209,7 +218,7 @@ def user_stats(df: pd.DataFrame) -> None:
     """Displays statistics on bikeshare users."""
 
     print("\nCalculating User Stats...\n")
-    start_time = time.time()
+    start_time: float = time.time()
 
     # Display counts of user types
     print("\n")
@@ -233,8 +242,8 @@ def user_stats(df: pd.DataFrame) -> None:
     print("-" * 40)
 
 
-def display_data(df: pd.DataFrame, display_stats: str) -> None:
-    buffer = 0
+def display_data(df: pd.DataFrame, display_stats: Any) -> None:
+    buffer: int = 0
     while display_stats.lower() == "yes":
         print(df.iloc[buffer : buffer + 5, :])
         buffer += 5
@@ -245,7 +254,7 @@ def display_data(df: pd.DataFrame, display_stats: str) -> None:
 
 def main() -> None:
     while True:
-        filtered_data = get_filters()
+        filtered_data: tuple = get_filters()
         if len(filtered_data) == 3:
             city, month, day = filtered_data
         else:
@@ -253,7 +262,7 @@ def main() -> None:
             month = "all"
             day = "all"
 
-        df = load_data(city, month, day)
+        df: pd.DataFrame = load_data(city, month, day)
 
         time_stats(df)
         station_stats(df)
@@ -264,7 +273,7 @@ def main() -> None:
         except KeyError:
             print("Sorry! No statistics on user gender.")
 
-        display_stats = input(
+        display_stats: str = input(
             "\nWould you like to see 5 line of the data? Enter yes or no: "
         )
         if display_stats.lower() != "yes":
@@ -272,7 +281,7 @@ def main() -> None:
         else:
             display_data(df, display_stats)
 
-        restart = input("\nWould you like to restart? Enter yes or no: \n")
+        restart: str = input("\nWould you like to restart? Enter yes or no: \n")
         if restart.lower() != "yes":
             break
 
